d2facc198b561d221d2aa037ad95f1918e73fc33
akincengiz <akincengiz@outlook.com.tr>
2025-04-07 13:21:07 +0300

    .

--- JavaScript/19-Fetch/fetch.js
+++ JavaScript/19-Fetch/fetch.js
@@ -1,43 +1,42 @@
 const listItem = document.querySelector("#list");
-function getTextData(fileUrl){
-    fetch(fileUrl)//TXT Dosyasına istek attık
-    .then(response => response.text())//Dosyadan gelen cevabı(response) text() metodu ile texte çevirdik
-    .then(data => console.log(data))//Text metodu ile gelen veriyi aldık ve console yazdık
-    .catch(err => console.log(err))
+function getTextData(fileUrl) {
+  fetch(fileUrl) //TXT Dosyasına istek attık
+    .then((response) => response.text()) //Dosyadan gelen cevabı(response) text() metodu ile texte çevirdik
+    .then((data) => console.log(data)) //Text metodu ile gelen veriyi aldık ve console yazdık
+    .catch((err) => console.log(err));
 }
 
-function getJsonData(fileUrl){
-    fetch(fileUrl)
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(err => console.log(err))
-
+function getJsonData(fileUrl) {
+  fetch(fileUrl)
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((err) => console.log(err));
 }
 
-function getApiData(url){
-    fetch(url)
-    .then(res => res.json())
-    .then(data => {
-        // data.forEach(item => {            
-        //     listItem.innerHTML += `
-        //     <li>${item.title} => Price : ${item.price}</li>
-        //     `
-        // });
-        let dataInfons = data.filter(item => item.price < 50);
-        dataInfons.forEach(item => {
-            listItem.innerHTML += `
+function getApiData(url) {
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      // data.forEach(item => {
+      //     listItem.innerHTML += `
+      //     <li>${item.title} => Price : ${item.price}</li>
+      //     `
+      // });
+      let dataInfons = data.filter((item) => item.price < 50);
+      dataInfons.forEach((item) => {
+        listItem.innerHTML += `
            <div class="card" style="width: 18rem;">
-  <img src="${item.image}" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">${item.title}</h5>
-    <p class="card-text">${item.description}</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
-            `
-        });
+            <img src="${item.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${item.title}</h5>
+                <p class="card-text">${item.description}</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+            </div>
+            `;
+      });
     })
-    .catch(err => console.log(err))
+    .catch((err) => console.log(err));
 }
 
 getTextData("text.txt");
