ddc9cdb750b7250b38054f81987d56554d313f24
akincengiz <akincengiz@outlook.com.tr>
2025-04-07 13:19:27 +0300

    fetch completed

--- JavaScript/19-Fetch/fetch.js
+++ JavaScript/19-Fetch/fetch.js
@@ -1,3 +1,4 @@
+const listItem = document.querySelector("#list");
 function getTextData(fileUrl){
     fetch(fileUrl)//TXT Dosyasına istek attık
     .then(response => response.text())//Dosyadan gelen cevabı(response) text() metodu ile texte çevirdik
@@ -16,7 +17,26 @@ function getJsonData(fileUrl){
 function getApiData(url){
     fetch(url)
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => {
+        // data.forEach(item => {            
+        //     listItem.innerHTML += `
+        //     <li>${item.title} => Price : ${item.price}</li>
+        //     `
+        // });
+        let dataInfons = data.filter(item => item.price < 50);
+        dataInfons.forEach(item => {
+            listItem.innerHTML += `
+           <div class="card" style="width: 18rem;">
+  <img src="${item.image}" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${item.title}</h5>
+    <p class="card-text">${item.description}</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+            `
+        });
+    })
     .catch(err => console.log(err))
 }
 
@@ -24,6 +44,11 @@ getTextData("text.txt");
 getTextData("text2.txt");
 
 getJsonData("data.json");
+
+getApiData("https://fakestoreapi.com/products");
+// getApiData("https://jsonplaceholder.typicode.com/todos");
+// getApiData("https://dummyjson.com/comments");
+// getApiData("http://example.com/articles");
 
 //https://fakestoreapi.com/products
 //https://jsonplaceholder.typicode.com/todos
