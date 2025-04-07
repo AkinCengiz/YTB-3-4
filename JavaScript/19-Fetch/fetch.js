6b6b6f42ac7334a6213aaa610faeeabdfa050fab
akincengiz <akincengiz@outlook.com.tr>
2025-04-07 12:26:59 +0300

    Fetch Started

---
+++ JavaScript/19-Fetch/fetch.js
@@ -0,0 +1,24 @@
+function getTextData(fileUrl){
+    fetch(fileUrl)//TXT Dosyasına istek attık
+    .then(response => response.text())//Dosyadan gelen cevabı(response) text() metodu ile texte çevirdik
+    .then(data => console.log(data))//Text metodu ile gelen veriyi aldık ve console yazdık
+    .catch(err => console.log(err))
+}
+
+function getJsonData(fileUrl){
+    fetch(fileUrl)
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(err => console.log(err))
+
+}
+
+getTextData("text.txt");
+getTextData("text2.txt");
+
+getJsonData("data.json");
+
+//https://fakestoreapi.com/products
+//https://jsonplaceholder.typicode.com/todos
+//https://dummyjson.com/comments
+//http://example.com/articles
