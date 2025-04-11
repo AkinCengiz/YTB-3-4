04779fef8c263a157187712f450f579b844e66f4
akincengiz <akincengiz@outlook.com.tr>
2025-04-11 12:46:22 +0300

    ..

--- React/03-Router-Dom/01-router-dom/src/App.jsx
+++ React/03-Router-Dom/01-router-dom/src/App.jsx
@@ -1,15 +1,23 @@
+import { Routes, Route } from "react-router-dom";
 import './App.css'
-// import AboutPage from './pages/AboutPage'
-// import HomePage from './pages/HomePage'
+import AboutPage from './pages/AboutPage'
+import HomePage from './pages/HomePage'
 import ContactPage from './pages/ContactPage'
+import NoContent from "./pages/NoContent";
 
 function App() {
 
   return (
     <>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/contact" element={<ContactPage />} />
+      <Route path="/about" element={<AboutPage />} />
+      <Route path="*" element={<NoContent/>}/>
+    </Routes>
       {/* <HomePage /> */}
       {/* <AboutPage /> */}
-      <ContactPage />
+      
     </>
   )
 }
---
+++ React/03-Router-Dom/01-router-dom/src/pages/NoContent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function NoContent() {
+  return (
+    <h1>Sayfa bulunamadı...</h1>
+  )
+}
+
+export default NoContent
